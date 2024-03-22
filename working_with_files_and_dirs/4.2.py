15a1ac7d92305f6a483d7d944d9574d31a08ba71
Ivan Andreev <142481501+AndreeWanya@users.noreply.github.com>
2024-03-22 21:31:13 +0700

    Add der if_flag

--- working_with_files_and_dirs/4.2.py
+++ working_with_files_and_dirs/4.2.py
@@ -1,4 +1,5 @@
 import os.path
+import logging
 
 
 def grep(path):
@@ -11,18 +12,32 @@ def grep(path):
     return dirs, files
 
 
+def if_flag(path, file_extension, flag):
+    my_list = list(grep(path))
+    logging.warning(f'dirs = {my_list[0]}, files = {my_list[1]}')
+    if flag == True:
+        dirs, files = [], []
+        for di in my_list[0]:
+            dirs_list, files_list = grep(path + '/' + di)
+            logging.warning(f'new dirs = {dirs_list}, new files = {files_list}')
+            dirs += dirs_list
+            files += files_list
+        my_list[0] += dirs
+        my_list[1] += files
+    my_list[1] = [fi for fi in my_list[1] if fi.split('.')[-1] == file_extension or file_extension == '*']
+    return my_list
+
+
 def del_dir(path):
     if os.path.isdir(path) == False:
         return False
-    dirs, files = grep(path)
-    if len(dirs) != 0:
+    dir_list = if_flag(path, '*', False)
+    if len(dir_list[0]) != 0:
         return False
-    for fi in files:
+    for fi in dir_list[1]:
         os.remove(path + '/' + fi)
     os.rmdir(path)
-    if os.path.isdir(path) == False:
-        return True
-    return False
+    return True
 
 print(del_dir('/home/andreewanya/Документы/Bobrovsky/py1/graduate-programming-school/working_with_files_and_dirs/New-folder'))         # Вернет Fasle, так как есть вложенная директория New-folder2
 print(del_dir('/home/andreewanya/Документы/Bobrovsky/py1/graduate-programming-school/working_with_files_and_dirs/New-folder/New-folder2'))         # Вернет True, так как нет вложенных директорий, но есть вложенные файлы
