f710f1443c61cbd09a02c84f24fd5eae624564df
Ivan Andreev <142481501+AndreeWanya@users.noreply.github.com>
2024-04-06 21:36:07 +0700

    Fix SynchronizingTables function

--- survived/SynchronizingTables.py
+++ survived/SynchronizingTables.py
@@ -1,6 +1,6 @@
 def SynchronizingTables(N: int, ids: list, salary: list) -> list:
-	ids_dict = dict(zip(ids, [i for i in range(N)]))
-	salary_dict = dict(zip([i for i in range(N)], salary))
+	ids_dict = dict(zip(sorted(ids), [i for i in range(N)]))
+	salary_dict = dict(zip([i for i in range(N)], sorted(salary)))
 	salary = []
 	for id in ids:
 		salary.append(salary_dict[ids_dict[id]])
