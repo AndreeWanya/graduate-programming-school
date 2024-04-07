72ee477e6e55ec20c12eb09aa4b5ceec9f3d6d02
Ivan Andreev <142481501+AndreeWanya@users.noreply.github.com>
2024-04-07 18:45:27 +0700

    Fix main algorithm

--- survived/Mobile_phone_unlocking.py
+++ survived/Mobile_phone_unlocking.py
@@ -2,8 +2,15 @@ from math import sqrt
 
 def PatternUnlock(N: int, hits: list) -> str:
     unlock_code = 0
+    points = ['654', '123', '987']
     for i in range(1, N):
-        if abs(hits[i] - hits[i-1]) > 1:
+        if str(hits[i]) in points[0] and str(hits[i-1]) in points[1] and points[0].find(str(hits[i])) != points[1].find(str(hits[i-1])):
+            unlock_code += sqrt(2)
+        elif str(hits[i]) in points[1] and str(hits[i-1]) in points[2] and points[1].find(str(hits[i])) != points[2].find(str(hits[i-1])):
+            unlock_code += sqrt(2)
+        elif str(hits[i]) in points[2] and str(hits[i-1]) in points[1] and points[2].find(str(hits[i])) != points[1].find(str(hits[i-1])):
+            unlock_code += sqrt(2)
+        elif str(hits[i]) in points[1] and str(hits[i-1]) in points[0] and points[1].find(str(hits[i])) != points[0].find(str(hits[i-1])):
             unlock_code += sqrt(2)
         else:
             unlock_code += 1
@@ -12,3 +19,4 @@ def PatternUnlock(N: int, hits: list) -> str:
     return unlock_code
 
 #print(PatternUnlock(10, [1, 2, 3, 4, 5, 6, 2, 7, 8, 9]))
+#print(PatternUnlock(3, [2, 1, 9]))
