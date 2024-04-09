d2e2bde89b242a6e15684a8160ebd3223b2ca85e
Ivan Andreev <142481501+AndreeWanya@users.noreply.github.com>
2024-04-09 16:05:21 +0700

    Fix WordSearch function

--- survived/Google_competitor.py
+++ survived/Google_competitor.py
@@ -8,16 +8,20 @@ def WordSearch(len_sub: int, s: str, subs: str) -> list:
     subs_list = []
     for i in range(len(s)):
         new_line += s[i]
+        if new_line == ' ':
+        	new_line = ''
         if len(new_line) == len_sub and s[i] == ' ':
-            subs_list.append(new_line)
+            subs_list.append(new_line.rstrip(' '))
             new_line = ''
         elif len(new_line) == len_sub and s[i] != ' ':
             if ' ' in new_line[::-1]:
                 subs_list.append(new_line[:-new_line[::-1].find(' ')-1])
                 new_line = new_line[-new_line[::-1].find(' '):]
             else:
-                subs_list.append(new_line[:len_sub-1] + '-')
-                new_line = new_line[len_sub:]
+                subs_list.append(new_line[:len_sub])
+                new_line = ''
+        elif i == len(s) - 1:
+        	subs_list.append(new_line)
     result_list = []
     for sub in subs_list:
         if subs in sub.split(' '):
@@ -28,3 +32,4 @@ def WordSearch(len_sub: int, s: str, subs: str) -> list:
 
 #print(WordSearch(12, '1) cnhjrf hfp,bdftncz yf yf,jh cnhjr xthtp dshfdybdfybt gj pflfyyjq ibhbyt/', 'cnhjr'))
 #print(WordSearch(10, '12345', 'subs'))
+#print(WordSearch(3, '12345', '123'))
