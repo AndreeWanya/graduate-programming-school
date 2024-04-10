7257ecd7761c57070be2d73302950f392138f01f
Ivan Andreev <142481501+AndreeWanya@users.noreply.github.com>
2024-04-10 12:30:04 +0700

    Fix WordSearch function

--- survived/Google_competitor.py
+++ survived/Google_competitor.py
@@ -4,32 +4,41 @@ def WordSearch(len_sub: int, s: str, subs: str) -> list:
             return [1]
         else:
             return [0]
+    query_lines = []
     new_line = ''
-    subs_list = []
+    s = s.split(' ')
     for i in range(len(s)):
-        new_line += s[i]
-        if new_line == ' ':
-        	new_line = ''
-        if len(new_line) == len_sub and s[i] == ' ':
-            subs_list.append(new_line.rstrip(' '))
-            new_line = ''
-        elif len(new_line) == len_sub and s[i] != ' ':
-            if ' ' in new_line[::-1]:
-                subs_list.append(new_line[:-new_line[::-1].find(' ')-1])
-                new_line = new_line[-new_line[::-1].find(' '):]
-            else:
-                subs_list.append(new_line[:len_sub])
-                new_line = ''
-        elif i == len(s) - 1:
-        	subs_list.append(new_line)
-    result_list = []
-    for sub in subs_list:
-        if subs in sub.split(' '):
-            result_list.append(1)
-        else:
-            result_list.append(0)
-    return result_list
-
-#print(WordSearch(12, '1) cnhjrf hfp,bdftncz yf yf,jh cnhjr xthtp dshfdybdfybt gj pflfyyjq ibhbyt/', 'cnhjr'))
-#print(WordSearch(10, '12345', 'subs'))
-#print(WordSearch(3, '12345', '123'))
+    	new_line += ' ' + s[i]
+    	new_line = new_line.lstrip(' ')
+    	if len(new_line) < len_sub - 1:
+    		if i == len(s) - 1:
+    			query_lines.append(new_line)
+    			new_line = ''
+    			break
+    		else:
+    			continue
+    	elif len(new_line) in [len_sub - 1, len_sub]:
+    		query_lines.append(new_line)
+    		new_line = ''
+    		continue
+    	while len(new_line) > len_sub:
+    		if ' ' in new_line:
+    			query_lines.append(new_line[:-new_line[::-1].find(' ')-1])
+    			new_line = new_line[-new_line[::-1].find(' '):]
+    		else:
+    			query_lines.append(new_line[0: len_sub])
+    			new_line = new_line[len_sub:]
+    if len(new_line) > 0:
+    	query_lines.append(new_line)
+    result = []
+    for sub in query_lines:
+    	if subs in sub.split(' '):
+    		result.append(1)
+    	else:
+    		result.append(0)
+    return result
+    
+    
+print(WordSearch(12, '1) stroka razbivaetsya na nabor strok cherez vyravnivanie po zadannoj shirine.', 'strok'))
+print(WordSearch(10, '12345', 'subs'))
+print(WordSearch(3, '12345', '123'))
