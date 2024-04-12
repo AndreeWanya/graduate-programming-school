c1ee0d0a646d61b3f4fb75d40e6ce20a9c8d4e9a
Ivan Andreev <142481501+AndreeWanya@users.noreply.github.com>
2024-04-12 22:46:46 +0700

    Fix 'no winner' output

--- survived/146_pecrents.py
+++ survived/146_pecrents.py
@@ -1,6 +1,6 @@
 def MassVote(N: int, Votes: list) -> str:
     pass
 
-print(MassVote(5, [60, 10, 10, 15, 5]))
-print(MassVote(3, [10, 15, 10]))
-print(MassVote(4, [30, 25, 30, 20]))
+print(MassVote(5, [60, 10, 10, 15, 5]))     # majority winner 1
+print(MassVote(3, [10, 15, 10]))            # minority winner 2
+print(MassVote(4, [111, 111, 110, 110]))    # no winner
