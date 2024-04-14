dd0e2202344e50211e8058ec21b0bea08b50648e
Ivan Andreev <142481501+AndreeWanya@users.noreply.github.com>
2024-04-14 19:58:42 +0700

    Fix MassVote function algorithm

--- survived/146_pecrents.py
+++ survived/146_pecrents.py
@@ -1,12 +1,13 @@
 def MassVote(N: int, Votes: list) -> str:
-    winner = 0
+    counter = 0
     for i in range(len(Votes)):
+        counter += 1
         if Votes[i] == max(Votes):
-            winner += 1
-    if max(Votes) * 100 / sum(Votes) >= 50 and winner == 1:
-        return 'majority winner ' + str(Votes.count(max(Votes)))
-    elif max(Votes) * 100 / sum(Votes) < 50 and winner == 1:
-        return 'minority winner ' + str(Votes.count(max(Votes)))
+            break
+    if max(Votes) * 100 / sum(Votes) >= 50 and Votes.count(max(Votes)) == 1:
+        return 'majority winner ' + str(counter)
+    elif max(Votes) * 100 / sum(Votes) < 50 and Votes.count(max(Votes)) == 1:
+        return 'minority winner ' + str(counter)
     else:
         return 'no winner'
 
@@ -15,3 +16,4 @@ def MassVote(N: int, Votes: list) -> str:
 #print(MassVote(4, [111, 111, 110, 110]))    # no winner
 #print(MassVote(8, [111, 111, 110, 110, 50, 60, 70, 80]))
 #print(MassVote(2, [111, 110]))
+#print(MassVote(5, [10, 10, 60, 15, 5]))
