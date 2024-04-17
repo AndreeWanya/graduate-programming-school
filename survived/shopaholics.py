35ac578d3a1ebd11d3e11b8b8e0fb547d0977bbd
Ivan Andreev <142481501+AndreeWanya@users.noreply.github.com>
2024-04-17 16:16:19 +0700

    Edit MaximumDiscount function

--- survived/shopaholics.py
+++ survived/shopaholics.py
@@ -1,1 +1,8 @@
 def MaximumDiscount(N: int, price: list) -> int:
+	price = sorted(price, reverse=True)
+	counter = 0
+	for i in range(2, N, 3):
+		counter += price[i]
+	return counter
+		
+#print(MaximumDiscount(7, [400, 350, 300, 250, 150, 200, 100]))
