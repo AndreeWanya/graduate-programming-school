e434a46fc529791a3162af7c1812465d549b7fbb
Ivan Andreev <142481501+AndreeWanya@users.noreply.github.com>
2024-04-18 19:43:06 +0700

    Add three_nums_sorting function

--- survived/Mr_Robot_and_the_Evil_Corporation.py
+++ survived/Mr_Robot_and_the_Evil_Corporation.py
@@ -1,12 +1,24 @@
+import time
+def three_nums_sorting(sub_data):
+	for i in range(2):
+		sub_data[2], sub_data[1], sub_data[0] = sub_data[0], sub_data[2], sub_data[1]
+		if sub_data[2] > sub_data[1] > sub_data[0]:
+			return sub_data
+	return sub_data
+
 def MisterRobot(N: int, data: list) -> bool:
-	data_copy = data.copy()
-	while True:
+	while (time.time() - start_time) < 1:
 		for i in range(2, N):
-			if data[i] < data[i-1]:
-				data[i], data[i-1], data[i-2] = data[i-1], data[i-2], data[i]
+			if data[i] < data[i - 1]:
+				print(data)
+				data = data[:i - 2] + three_nums_sorting(data[i - 2: i + 1]) + data[i + 1:]
+				print(data)
+				print()
 			if data == sorted(data):
 				return True
-		if data == data_copy:
-			return False
+	return False
+
+start_time = time.time()
 
 #print(MisterRobot(7, [1, 3, 4, 5, 6, 2, 7]))
+#print(MisterRobot(10, [9, 8, 7, 6, 5, 4, 3, 2, 1, 10]))
