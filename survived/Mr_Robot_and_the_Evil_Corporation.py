a58efa257de68b153e9b210af88d7c9a0230f053
Ivan Andreev <142481501+AndreeWanya@users.noreply.github.com>
2024-04-18 18:05:48 +0700

    Add if condition for False result

--- survived/Mr_Robot_and_the_Evil_Corporation.py
+++ survived/Mr_Robot_and_the_Evil_Corporation.py
@@ -1,9 +1,12 @@
 def MisterRobot(N: int, data: list) -> bool:
+	data_copy = data.copy()
 	while True:
 		for i in range(2, N):
 			if data[i] < data[i-1]:
 				data[i], data[i-1], data[i-2] = data[i-1], data[i-2], data[i]
 			if data == sorted(data):
 				return True
+		if data == data_copy:
+			return False
 
-print(MisterRobot(7, [1, 3, 4, 5, 6, 2, 7]))
+#print(MisterRobot(7, [1, 3, 4, 5, 6, 2, 7]))
