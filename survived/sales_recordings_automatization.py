98a0fda26116f37cb3ad98c5b2215b7b4b1d5ce3
Ivan Andreev <142481501+AndreeWanya@users.noreply.github.com>
2024-04-19 15:33:56 +0700

    Fix ShopOLAP function

--- survived/sales_recordings_automatization.py
+++ survived/sales_recordings_automatization.py
@@ -1,4 +1,18 @@
 def ShopOLAP(N: int, items: list) -> list:
+    items = sorted(items)
+    del_items = []
+    for i in range(N-1):
+    	if items[i].split()[0] == items[i+1].split()[0]:
+    		items[i] = items[i].split()[0] + ' ' + str(int(items[i].split()[1]) + int(items[i+1].split()[1]))
+    		del_items.append(i+1)
+    for item in del_items:
+    	del items[item]
+    sorted_items = {}
+    for item in items:
+    	sorted_items[int(item.split()[1])] = []
+    for item in items:
+    	sorted_items[int(item.split()[1])].append(item.split()[0])
+    print(sorted_items)
     return items
 
 print(ShopOLAP(5, ['платье1 5', 'сумка32 2', 'платье1 1', 'сумка23 2', 'сумка128 4']))
