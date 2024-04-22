1a3436a882cb8787499536db35a837718e2e2589
Ivan Andreev <142481501+AndreeWanya@users.noreply.github.com>
2024-04-22 08:24:34 +0700

    Edit BastShoe function algorithm

--- survived/national_editor_bast_shoe.py
+++ survived/national_editor_bast_shoe.py
@@ -1,28 +1,34 @@
 def BastShoe(command: str) -> str:
+    global actual_line, lines_history
     if command[0] == '1':
-        return commands_history[-1] + command[2:]
+        actual_line += command[2:]
+        lines_history.append(actual_line)
     elif command[0] == '2':
-        if int(command[2:]) > len(commands_history[-1]):
-            return ''
+        if int(command[2:]) > len(actual_line):
+            actual_line = ''
         else:
-            return commands_history[-1][:-2]
+            actual_line = actual_line[:-int(command[2:])]
+            #print(lines_history)
+        lines_history.append(actual_line)
     elif command[0] == '3':
-        if int(command[2:]) > len(commands_history[-1]):
+        if int(command[2:]) > len(actual_line):
             return ''
         else:
-            return commands_history[-1][int(command[2:])]
+            return actual_line[int(command[2:])]
     elif command[0] == '4':
-        pass
+        #print(lines_history)
+        if len(lines_history) >= 1:
+        	actual_line = lines_history[-2]
+        	lines_history.append(actual_line)
     elif command[0] == '5':
         pass
-    else:
-        return actual_line
+    return actual_line
 
 commands = ['1 Привет', '1 , Мир!', '1 ++', '2 2', '4', '4', '1 *', '4', '4', '4', '3 6', '2 100',
             '1 Привет', '1 , Мир!', '1 ++', '4', '4', '5', '4', '5', '5', '5', '5', '4', '4', '2 2',
             '4', '5', '5', '5']
 
-commands_history = ['']
+lines_history = []
 actual_line = ''
 for command in commands:
     print(BastShoe(command))
