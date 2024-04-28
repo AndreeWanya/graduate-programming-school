1082ad5abe8edf504f0d653d98639009fe1d820a
Ivan Andreev <142481501+AndreeWanya@users.noreply.github.com>
2024-04-28 15:28:44 +0700

    Edit MatrixTurn function algorithm

--- survived/the_matrix_rotation.py
+++ survived/the_matrix_rotation.py
@@ -1,34 +1,24 @@
 def MatrixTurn(Matrix: list, M: int, N: int, T: int) -> None:
-    mtrx_of_strs = []
-    for i in range(int(M/2)):
-    	mtrx_to_str = Matrix[i][i:N-i]
-    	if i+1 == M/2:
-    		mtrx_to_str += ''.join(reversed(Matrix[-i-1][i:N-i]))
-    		mtrx_of_strs.append(mtrx_to_str)
-    		continue
-    	for j in range(M-2):
-    		mtrx_to_str += Matrix[j+1][-i-1]
-    	mtrx_to_str += ''.join(reversed(Matrix[-i-1][i:N-i]))
-    	for j in range(M-2):
-    		mtrx_to_str += Matrix[-j-2][i]
-    	mtrx_of_strs.append(mtrx_to_str)
-    
-    for i in range(len(mtrx_of_strs)):
-    	mtrx_of_strs[i] = mtrx_of_strs[i][-1] + mtrx_of_strs[i][:-1]
-    	Matrix[i] = Matrix[i][:i+1] + mtrx_of_strs[i][:N-i] + Matrix[i][N-i:]
-    	if i+1 == len(mtrx_of_strs):
-    		Matrix[-i-1] = Matrix[-i-1][i] + ''.join(reversed(mtrx_of_strs[i][N-i:-N-i-1]))
-    		print(Matrix)
-    		continue
-    	for j in range(len(mtrx_of_strs)):
-    		Matrix[j+1] = Matrix[j+1][:-i-1] + mtrx_of_strs[i][N+ j]
-    		print(Matrix)
-    	print()
-    	print(mtrx_of_strs[i])
-    	Matrix[-i-1] = ''.join(reversed(mtrx_of_strs[i][N + len(mtrx_of_strs):N + len(mtrx_of_strs) + N]))
-    	for j in range(len(mtrx_of_strs)):
-    		Matrix[j+1] = Matrix[j+1][:i] + mtrx_of_strs[i][N + len(mtrx_of_strs) + j] + Matrix[j+1][i+1:]
-    print(Matrix)
-    	
-      	   	
-MatrixTurn(['123456', '234567', '345678', '456789'], 4, 6, 3)
+    if M > N:
+        main_size = N
+    else:
+        main_size = M
+    for t in range(T):
+        mtrx = [list(x) for x in Matrix]
+        for i in range(int(main_size / 2)):
+            num = Matrix[1][0]
+            for j in range(len(Matrix[0])):
+                mtrx[i][i + j], num = num, Matrix[0][j]
+            for j in range(len(Matrix) - 2):
+                mtrx[i + j + 1][-i - 1], num = num, Matrix[j + 1][-1]
+            for j in range(len(Matrix[0])):
+                mtrx[-i - 1][-i - j - 1], num = num, Matrix[-1][-j - 1]
+            for j in range(len(Matrix) - 2):
+                mtrx[-i - j - 2][i], num = num, Matrix[-j - 2][0]
+            Matrix = [x[1:-1] for x in Matrix[1:-1]]
+        Matrix = [''.join(x) for x in mtrx]
+
+# MatrixTurn(['123456', '234567', '345678', '456789'], 4, 6, 3)
+# MatrixTurn(['123456', '234567', '345678', '456789', '567890', '678901'], 6, 6, 3)
+# MatrixTurn(['123456', '234567', '345678', '456789', '567890', '678901', '456789', '987654'], 8, 6, 3)
+# MatrixTurn(['12345', '23456', '34567', '45678', '56789', '67890'], 6, 5, 3)
