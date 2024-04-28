75c8302756b9d141ec6c27c02dfa661624e50bef
Ivan Andreev <142481501+AndreeWanya@users.noreply.github.com>
2024-04-28 23:04:21 +0700

    Fix the circle limit

--- survived/Chewbacca_and_gravitational_wells.py
+++ survived/Chewbacca_and_gravitational_wells.py
@@ -1,14 +1,15 @@
-def TransformTransform(A: list, N: int) -> bool:
+def one_transform(A: list) -> list:
     B = []
-    for i in range(len(A) - 1):
-        for j in range(len(A) - i - 1):
+    for i in range(len(A)):
+        for j in range(len(A) - i):
             k = i + j
             B.append(max(A[j: k + 1]))
-    S = []
-    for i in range(len(B) - 1):
-        for j in range(len(B) - i - 1):
-            k = i + j
-            S.append(max(B[j: k + 1]))
+    return B
+
+def TransformTransform(A: list, N: int) -> bool:
+    S = one_transform(one_transform(A))
+    # print(sum(S))
     return sum(S) % 2 == 0
 
 # print(TransformTransform([3, 2, 1], 3))
+# print(TransformTransform([1,2,1,7,2,4,3,1,5,1,2,1,6,1,2], 15))
