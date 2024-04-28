1ed288366f7315aad3dc5ed00808743f43fca5be
Ivan Andreev <142481501+AndreeWanya@users.noreply.github.com>
2024-04-28 21:17:06 +0700

    Fix the right limit of array

--- survived/Chewbacca_and_gravitational_wells.py
+++ survived/Chewbacca_and_gravitational_wells.py
@@ -3,10 +3,12 @@ def TransformTransform(A: list, N: int) -> bool:
     for i in range(len(A) - 1):
         for j in range(len(A) - i - 1):
             k = i + j
-            B.append(max(A[j: k]))
+            B.append(max(A[j: k + 1]))
     S = []
     for i in range(len(B) - 1):
         for j in range(len(B) - i - 1):
             k = i + j
-            S.append(max(B[j: k]))
+            S.append(max(B[j: k + 1]))
     return sum(S) % 2 == 0
+
+# print(TransformTransform([3, 2, 1], 3))
