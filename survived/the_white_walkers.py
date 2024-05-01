7b343485a7b50a2ba3fced22110012693d696868
Ivan Andreev <142481501+AndreeWanya@users.noreply.github.com>
2024-05-01 20:02:17 +0700

    Edit white_walkers algorithm

--- survived/the_white_walkers.py
+++ survived/the_white_walkers.py
@@ -1,2 +1,18 @@
 def white_walkers(village: str) -> bool:
+    first = [0, 0]
+    flag = False
+    for i in range(len(village)):
+        if village[i].isdigit() and int(first[0]) + int(village[i]) == 10:
+            if village[first[1]: i].count('=') == 3:
+                flag = True
+                first = [village[i], i]
+            else:
+                return False
+        elif village[i].isdigit():
+            first = [village[i], i]
+    return flag
 
+# villages = ['axxb6===4xaf5===eee5', '5==ooooooo=5=5', 'abc=7==hdjs=3gg1=======5',
+#             'aaS=8', '9===1===9===1===9']
+# for village in villages:
+#     print(white_walkers(village))
