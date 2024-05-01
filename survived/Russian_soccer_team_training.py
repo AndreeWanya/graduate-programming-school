7975f9c54a6ce8da795103b446c930e8b5287085
Ivan Andreev <142481501+AndreeWanya@users.noreply.github.com>
2024-05-01 21:55:38 +0700

    Add file 'Russian_soccer_team_training.py'

---
+++ survived/Russian_soccer_team_training.py
@@ -0,0 +1,14 @@
+def Football(F: list, N: int) -> bool:
+    if list(reversed(F)) == sorted(F):
+        return True
+    for i in range(N-1):
+        for j in range(i+1, N):
+            if F[:i] + [F[j]] + F[i+1:j] + [F[i]] + F[j+1:] == sorted(F):
+                return True
+            elif F[:i] + list(reversed(F[i:j])) + F[j:] == sorted(F):
+                return True
+    return False
+
+# examples = [[1, 3, 2], [3, 2, 1], [1, 7, 5, 3, 9], [9, 5, 3, 7, 1], [1, 4, 3, 2, 5]]
+# for example in examples:
+#     print(Football(example, len(example)))
