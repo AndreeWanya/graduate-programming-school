47f1797120b70b18631813b160d2612c8e017e23
Ivan Andreev <142481501+AndreeWanya@users.noreply.github.com>
2024-05-01 22:23:00 +0700

    Add if condition for F == sorted(F)

--- survived/Russian_soccer_team_training.py
+++ survived/Russian_soccer_team_training.py
@@ -1,6 +1,8 @@
 def Football(F: list, N: int) -> bool:
     if list(reversed(F)) == sorted(F):
         return True
+    if F == sorted(F):
+        return False
     for i in range(N-1):
         for j in range(i+1, N):
             if F[:i] + [F[j]] + F[i+1:j] + [F[i]] + F[j+1:] == sorted(F):
@@ -9,6 +11,6 @@ def Football(F: list, N: int) -> bool:
                 return True
     return False
 
-# examples = [[1, 3, 2], [3, 2, 1], [1, 7, 5, 3, 9], [9, 5, 3, 7, 1], [1, 4, 3, 2, 5]]
+# examples = [[1, 3, 2], [3, 2, 1], [1, 7, 5, 3, 9], [9, 5, 3, 7, 1], [1, 4, 3, 2, 5], [1, 2, 3]]
 # for example in examples:
 #     print(Football(example, len(example)))
