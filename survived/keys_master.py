a071a7e88ebecb44c119b967eaf3a890c514061d
Ivan Andreev <142481501+AndreeWanya@users.noreply.github.com>
2024-05-02 20:50:20 +0700

    Edit Keymaker functions algorithm

--- survived/keys_master.py
+++ survived/keys_master.py
@@ -1,6 +1,14 @@
 def Keymaker(k: int) -> str:
-    pass
+    doors = ['1' for x in range(k)]
+    for i in range(1, k):
+        for j in range(i, k, i+1):
+            if doors[j] == '0':
+                doors[j] = '1'
+            else:
+                doors[j] = '0'
+    return ''.join(doors)
 
-doors_quantity = [1, 2, 3, 4, 5, 10, 15, 20, 50, 100]
-for doors in doors_quantity:
-    print(Keymaker(doors))
+# doors_quantity = [1, 2, 3, 4, 5, 10, 25, 100]
+# for doors in doors_quantity:
+#     print(Keymaker(doors))
+
