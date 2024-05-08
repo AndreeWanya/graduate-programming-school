99b76847194a160038b85a0f3657945a3f200272
Ivan Andreev <142481501+AndreeWanya@users.noreply.github.com>
2024-05-08 22:16:51 +0700

    Fix elif to if

--- recursion_8_tasks/recursion_1.py
+++ recursion_8_tasks/recursion_1.py
@@ -1,9 +1,9 @@
 def x_power_of_num(num: float, x: int) -> float:
 	if x == 0:
 		return 1
-	elif x == 1:
+	if x == 1:
 		return num
 	# x -= 1
 	return num * x_power_of_num(num, x - 1)
 	
-# print(x_power_of_num(2, 5))
+# print(x_power_of_num(2, 4))
