aad7f81a92a25b36924f24040bead53e3e49bee0
Ivan Andreev <142481501+AndreeWanya@users.noreply.github.com>
2024-05-09 20:53:11 +0700

    Del length for more demonstrativeness

--- recursion_8_tasks/recursion_3.py
+++ recursion_8_tasks/recursion_3.py
@@ -1,9 +1,8 @@
 def lists_length(lst: list) -> int:
-    length = 1
     if len(lst) == 1:
-        return length
+        return 1
     lst.pop(0)
-    return length + lists_length(lst)
+    return 1 + lists_length(lst)
 
 
 # lists = [[1, 2, 3, 4, 5], ['a', 'b', 'c', 'd'], [[0], [0], [0], [0], [0], [0]]]
