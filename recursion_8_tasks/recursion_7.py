1118224bc53a1986b4f9a2cc581f823c73099478
Ivan Andreev <142481501+AndreeWanya@users.noreply.github.com>
2024-05-12 20:14:01 +0700

    Edit second_max function algorithm

--- recursion_8_tasks/recursion_7.py
+++ recursion_8_tasks/recursion_7.py
@@ -1,6 +1,15 @@
 def second_max(nums: list) -> int:
-    pass
+    return second_max_cont(nums, 0, 0, 0)
 
-nums_lists = [[5, 4, 3, 2, 5], [2, 3, 4, 5]]
-for nums in nums_lists:
-    print(second_max(nums))
+def second_max_cont(nums: list, f_max: int, s_max: int, ind: int) -> int:
+    if ind == len(nums):
+        return s_max
+    if nums[ind] >= f_max:
+        s_max = f_max
+        f_max = nums[ind]
+    return second_max_cont(nums, f_max, s_max, ind + 1)
+
+
+# nums_lists = [[5, 4, 3, 2, 5], [2, 3, 4, 5]]
+# for nums in nums_lists:
+#     print(second_max(nums))
