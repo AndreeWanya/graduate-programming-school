b63f64693988f9dce283cbfcdb89f231993d4604
Ivan Andreev <142481501+AndreeWanya@users.noreply.github.com>
2024-05-12 19:48:27 +0700

    Add print instruction to even_values_second func

--- recursion_8_tasks/recursion_5.py
+++ recursion_8_tasks/recursion_5.py
@@ -1,14 +1,14 @@
-def even_values_second(nums: list, length: int) -> list:
-    if length == 0:
+def even_values_second(nums: list, counter: int) -> list:
+    if counter == len(nums):
         return nums
-    if nums[-length] % 2 != 0:
-        nums.pop(-length)
-    return even_values_second(nums, length - 1)
+    if nums[counter] % 2 == 0:
+        print(nums[counter])
+    return even_values_second(nums, counter + 1)
 
 def even_values(nums: list) -> list:
-    return even_values_second(nums, len(nums))
+    return even_values_second(nums, 0)
 
 
 # nums_lists = [[1, 2, 3, 4, 5, 6, 7, 8, 9], [2, 4, 8, 3], [1, 3, 5, 8], [1, 3, 5, 7]]
 # for lst in nums_lists:
-#     print(even_values(lst))
+#     even_values(lst)
