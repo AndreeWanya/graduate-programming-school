1eafd66d9044ba10913db654637da1576ee416c8
Ivan Andreev <142481501+AndreeWanya@users.noreply.github.com>
2024-05-17 16:42:13 +0700

    Edit grep_recursion function algorithm

--- recursion_8_tasks/recursion_8.py
+++ recursion_8_tasks/recursion_8.py
@@ -1,6 +1,18 @@
-def files_search(f_path: str) -> list:
-	pass
-	
-	
-folder_path = input()
-print(files_search(folder_path))
+import os.path
+
+def grep(path: str) -> list:
+    dirs, files = [], []
+    return grep_recursion(path, dirs, files, -1)
+
+def grep_recursion(path: str, dirs: list, files: list, ind: int) -> list:
+    for f in os.listdir(path):
+        if os.path.isdir(os.path.join(path, f)):
+            dirs.append(os.path.join(path, f))
+        else:
+            files.append(f)
+    ind += 1
+    if ind == len(dirs):
+        return files
+    return grep_recursion(dirs[ind], dirs, files, ind)
+
+# print(grep('./'))
