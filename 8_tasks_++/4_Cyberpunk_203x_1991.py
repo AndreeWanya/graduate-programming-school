20321d6b2d33d54582de326419451b28afb66097
Ivan Andreev <142481501+AndreeWanya@users.noreply.github.com>
2024-05-27 21:03:26 +0700

    Add file '4_Cyberpunk_203x_1991.py'

---
+++ 8_tasks_++/4_Cyberpunk_203x_1991.py
@@ -0,0 +1,6 @@
+def artificial_muscle_fibers(arr: list) -> int:
+    pass
+
+arrs = [[1, 2, 3, 4, 5], [1, 2, 3, 2, 1], [1, 2, 3, 2, 1, 2, 4, 2, 1]]
+for arr in arrs:
+    print(artificial_muscle_fibers(arr))
