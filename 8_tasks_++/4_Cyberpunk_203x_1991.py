e02388d2bda0c791288f97e8808c8307e75a1e66
Ivan Andreev <142481501+AndreeWanya@users.noreply.github.com>
2024-05-29 13:59:31 +0700

    Edit artificial_muscle_fibers function algorithm

--- 8_tasks_++/4_Cyberpunk_203x_1991.py
+++ 8_tasks_++/4_Cyberpunk_203x_1991.py
@@ -1,6 +1,12 @@
-def artificial_muscle_fibers(arr: list) -> int:
-    pass
+import sys
 
-arrs = [[1, 2, 3, 4, 5], [1, 2, 3, 2, 1], [1, 2, 3, 2, 1, 2, 4, 2, 1]]
+def artificial_muscle_fibers(arr: list) -> int:
+    nums_arr = []
+    for i in range(len(arr)):
+    	if arr[i] in arr[:i] and arr[i] not in nums_arr:
+    		nums_arr.append(arr[i])
+    return len(nums_arr), sys.getsizeof(nums_arr)
+
+arrs = [[1, 2, 3, 4, 5], [1, 2, 3, 2, 1], [1, 2, 3, 2, 1, 2, 4, 2, 1], [32000, 22, 546]]
 for arr in arrs:
     print(artificial_muscle_fibers(arr))
