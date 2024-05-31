9b898cee2e64630cce5c1e2f84cf7298c5edd30e
Ivan Andreev <142481501+AndreeWanya@users.noreply.github.com>
2024-05-31 21:51:30 +0700

    Commit executable code

--- 8_tasks_++/5_Cyberpunk_203x_1992.py
+++ 8_tasks_++/5_Cyberpunk_203x_1992.py
@@ -2,12 +2,12 @@ def massdriver(activate: list) -> int:
     my_dict = {}
     result = -1
     for i in range(len(activate)):
-    	if activate[i] not in my_dict:
-    		my_dict[activate[i]] = i
-    	elif my_dict[activate[i]] < result or result == -1:
-    		result = my_dict[activate[i]]
+        if activate[i] not in my_dict:
+            my_dict[activate[i]] = i
+        elif my_dict[activate[i]] < result or result == -1:
+            result = my_dict[activate[i]]
     return result
 
-arrs = [[1, 2, 3, 1, 2, 3, 4], [1, 2, 3, 4, 3, 4, 2], [1, 2, 3, 4, 5, 6, 7]]
-for arr in arrs:
-    print(massdriver(arr))
+# arrs = [[1, 2, 3, 1, 2, 3, 4], [1, 2, 3, 4, 3, 4, 2], [1, 2, 3, 4, 5, 6, 7]]
+# for arr in arrs:
+#     print(massdriver(arr))
