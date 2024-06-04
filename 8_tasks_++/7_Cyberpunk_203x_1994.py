7b309637c18e8bd012366dc8355b4feda767e31e
Ivan Andreev <142481501+AndreeWanya@users.noreply.github.com>
2024-06-04 21:32:17 +0700

    Add file "7_Cyberpunk_203x_1994.py"

---
+++ 8_tasks_++/7_Cyberpunk_203x_1994.py
@@ -0,0 +1,9 @@
+def matrix(n: int, m: int, matrix: list) -> list:
+	lst_of_mtrx = []
+	for i in range(m):
+		for j in range(n):
+			pass
+	return lst_of_mtrx
+	
+mtrx = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]
+print(matrix(len(mtrx[0]), len(mtrx), mtrx))
