33647e3efbd7add7b88df852d0947c0d880c55c6
Ivan Andreev <142481501+AndreeWanya@users.noreply.github.com>
2024-06-04 19:08:59 +0700

    Edit TRC_sort function algorithm

--- 8_tasks_++/6_Cyberpunk_203x_1993.py
+++ 8_tasks_++/6_Cyberpunk_203x_1993.py
@@ -1,6 +1,12 @@
 def TRC_sort(trc: list) -> list:
-    pass
+    sorted_trc = []
+    for i in range(len(trc)):
+    	if trc[i] == 0:
+    		sorted_trc = [0] + sorted_trc
+    	elif trc[i] == 1:
+    		sorted_trc.append(1)
+    return sorted_trc + [2] * (len(trc) - len(sorted_trc))
 
-arrs = [[2, 1, 0], [0, 1, 2, 1, 0, 2]]
-for arr in arrs:
-    print(TRC_sort(arr))
+#arrs = [[2, 1, 0], [0, 1, 2, 1, 0, 2], [1, 0, 1, 0, 2]]
+#for arr in arrs:
+#    print(TRC_sort(arr))
