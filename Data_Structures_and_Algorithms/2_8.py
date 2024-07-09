b2fa35bc837d46612619b38c1c1325a19bccfc34
Ivan Andreev <142481501+AndreeWanya@users.noreply.github.com>
2024-07-09 20:56:37 +0700

    Fix insert() method algorithm

--- Data_Structures_and_Algorithms/2_8.py
+++ Data_Structures_and_Algorithms/2_8.py
@@ -37,24 +37,7 @@ class LinkedList2:
         return result
 
     def delete(self, val, all=False):
-        node = self.head
-        while node is not None:
-            if node.value == val:
-                if self.head == self.tail:
-                    self.head = None
-                    self.tail = None
-                    node = node.next
-                elif node.next is not None:
-                    node.value = node.next.value
-                    node.next = node.next.next
-                    if all == False:
-                        node = None
-                else:
-                    node.prev.next = None
-                    self.tail = node.prev
-                    node = None
-            else:
-                node = node.next            
+        pass            
 
     def clean(self):
         self.head = None
@@ -71,9 +54,12 @@ class LinkedList2:
     def insert(self, afterNode, newNode):
         if afterNode is not None:
             afterNode = afterNode.value
-        if afterNode is None and self.head is None:
-            self.head = newNode
-            self.tail = newNode
+        if afterNode is None:
+            if self.head is None:
+                self.head = newNode
+                self.tail = newNode
+            else:
+                self.tail.next = newNode
         else:
             node = self.head
             while node is not None:
@@ -81,8 +67,7 @@ class LinkedList2:
                     newNode.next = node.next
                     node.next = newNode
                     break
-                elif afterNode is None or (node.value == afterNode and node.next is None):
-                    node.next = newNode
+                elif node.value == afterNode and node.next is None:
                     self.tail = newNode
                     break
                 else:
